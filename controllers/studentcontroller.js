abfca679aab3cb7918182cd745d2007b009b2cac
Devangsingh1969 <dev.work1969@gmail.com>
2024-09-23 20:44:55 +0530

    sended...

--- controllers/studentcontroller.js
+++ controllers/studentcontroller.js
@@ -5,7 +5,7 @@ async function addStudent(req,res){
        console.log(req.body);
     let student = new Student(req.body);
     await student.save();
-    res.render('studentinsert')
+    res.render('studentinsert.ejs')
     //    res.end("DATA IS ADDED SUCCESSFULLY...")
     }catch(err){
         console.log(err)
@@ -14,14 +14,30 @@ async function addStudent(req,res){
 async function getStudents(req,res){
     try{
         let students = await  Student.find({});
-        res.render('studentlist',{
+        res.render('studentlist.ejs',{
             students: students
         })
     }catch{
         console.log(err);
     }
 }
+async function getStudentsForEdit(req,res) {
+    try{
+        let id = req.params.id;
+        console.log(id)
+        let student = await Student.findOne({ _id: id})
+        console.log(student);
+        res.render('studentforupdate',{
+            student: student
+
+        })
+    }catch(err){
+        console.log(err)
+    }
+    
+}
 module.exports = {
+    getStudentsForEdit,
     getStudents,
     addStudent
 }
